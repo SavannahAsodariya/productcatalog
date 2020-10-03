5a8d9c99fbce50aa5d30b6d1c5d5bc45adcfcbb8
unknown <savanasodariya@gmail.com>
2020-10-03 10:40:30 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import ProductList from './Products/ProductList/ProductList';
-import Header from './Header';
 
 const App = () => {
     return (
         <div className="ui container">
-          <Header />
           <ProductList />
         </div>
     );
